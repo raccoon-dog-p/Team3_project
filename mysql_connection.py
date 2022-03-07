a31b97c086903f2a42bfe2813ad22b2583b61559
pys <qkrdydtjr02@naver.com>
2022-03-07 16:47:36 +0900

    test

--- mysql_connection.py
+++ mysql_connection.py
@@ -5,6 +5,6 @@ def get_connection():
     connection = mysql.connector.connect(
         host = 'my-db.cfbeyhqoz6o8.ap-northeast-2.rds.amazonaws.com',
         database = 'Project_db',
-        user = 'project_user',
+        user = 'project_user_1',
         password = '1234')
     return connection
