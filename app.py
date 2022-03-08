ceb0ec6cd6a5d2fbf314e1d1f1d36e8b6559fc11
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2022-03-08 11:19:52 +0900

    add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from http import HTTPStatus
 
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from movie import AddFavoriteResource, DeleteFavoriteResource, FavoriteListResource, MovieListResource, MovieSearchResource
+from movie import AddFavoriteResource, DeleteFavoriteResource, FavoriteListResource, MovieInfoResource, MovieListResource, MovieSearchResource
 from resources.login import UserLoginResource
 from resources.register import UserRegisterResource
 from resources.logout import UserLogoutResource, jwt_blacklist
@@ -33,6 +33,7 @@ api.add_resource(UserRegisterResource, '/api/register')
 api.add_resource(UserLoginResource, '/api/login')
 api.add_resource(UserLogoutResource, '/api/v1/user/logout')
 api.add_resource(MovieListResource,'/api/movie')
+api.add_resource(MovieInfoResource,'/5/<int:movie_id>')
 api.add_resource(MovieSearchResource,'/1')
 api.add_resource(AddFavoriteResource,'/2/<int:movie_id>')
 api.add_resource(FavoriteListResource,'/4')
--- movie.py
+++ movie.py
@@ -75,6 +75,47 @@ class MovieListResource(Resource):
         
         return {'count':len(record_list),'movie_list':record_list}  
         
+class MovieInfoResource(Resource):
+    def get(self,movie_id):
+        try :
+            connection = get_connection()
+
+            query = '''select id,title,short_description,genre_ids,release_year,urls,poster,provider
+                        from movie
+                        where id = %s;'''
+            
+            param = (movie_id,)
+            
+            cursor = connection.cursor(dictionary = True)
+            
+            cursor.execute(query,param)
+            
+            # select 문은 아래 내용이 필요하다.
+            record_list = cursor.fetchall()
+            print(record_list)
+            
+            ### 중요. 파이썬의 시간을, JSON으로 보내기 위해서
+            ### 문자열로 바꿔준다.
+            i = 0
+            for record in record_list :
+                record_list[i]['release_year'] = str(record['release_year'])
+                
+                i = i+1
+        # 위의 코드를 실행하다가, 문제가 생기면, except를 실행하라는 뜻.
+        except Error as e  : 
+            print('Error while connecting to MySQL',e)
+            return {'error':str(e)},HTTPStatus.BAD_REQUEST
+        # finally 는 try에서 에러가 나든 안나든, 무조건 실행하라는 뜻.    
+        finally :
+            cursor.close()
+            if connection.is_connected():
+                connection.close()
+                print('MySQL connection is closed')
+            else:
+                print('connection does not exist')
+        
+        return {'영화정보':record_list[0]}
+
 
 class MovieSearchResource(Resource):
     
