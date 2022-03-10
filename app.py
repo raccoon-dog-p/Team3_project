e51f7a6e3ec785b642b8479a13cb9263a5fe3a1a
pys <qkrdydtjr02@naver.com>
2022-03-10 16:32:39 +0900

    rating api first

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, request
 from config import Config
 from flask.json import jsonify
 from http import HTTPStatus
-
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from movie import AddFavoriteResource, DeleteFavoriteResource, MovieSearchResource
 from resources.login import UserLoginResource
+from resources.rating import MovieRecommandResource
 from resources.register import UserRegisterResource
 from resources.logout import UserLogoutResource, jwt_blacklist
 
@@ -35,6 +35,7 @@ api.add_resource(UserLogoutResource, '/api/v1/user/logout')
 api.add_resource(MovieSearchResource,'/1')
 api.add_resource(AddFavoriteResource,'/2/<int:movie_id>')
 api.add_resource(DeleteFavoriteResource,'/3/<int:movie_id>')
+api.add_resource(MovieRecommandResource,'/4')
 
 if __name__ == '__main__' :
     app.run()
