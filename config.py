efa57c0814d1ce749ca59434a96dbf0ac7d5aea5
pys <88516240+raccoon-dog-p@users.noreply.github.com>
2023-05-01 21:46:09 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config :
-    JWT_SECRET_KEY = 'yh@1234'
-    S3_BUCKET = "pys9-csvfiles"
-    S3_KEY = "AKIAY2KXTZ5XALFXA2MU"
-    S3_SECRET = "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"
+    JWT_SECRET_KEY = 'tset'
+    S3_BUCKET = "csvfiles"
+    S3_KEY = "don't public"
+    S3_SECRET = "don't public"
     S3_LOCATION = 'https://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
